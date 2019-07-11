4d4cf742aaa1a6de95ba50c797c7be2908dace37
gwinpin <crypt2creepy@gmail.com>
2019-07-11 11:32:26 +0200

    Mimimi transit

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -21,20 +21,27 @@ object Utils {
         return when {
             fn != null && ln != null -> "${fn.toUpperCase()}${ln.toUpperCase()}"
             fn != null -> "${fn.toUpperCase()}"
+            ln != null -> "${ln.toUpperCase()}"
             else -> null
         }
     }
 
 
-    fun transliteration(payload: String?, divider: String? = " "):String{
+    fun transliteration(payload: String?, divider: String = " "): String {
+        val parts: List<String>? = payload?.trim()?.split(divider)
 
-        val(firstName,lastName) = parseFullName(payload)
-        return "${this.translit(firstName)}$divider${this.translit(lastName)}"
+        var txt = ""
+        parts?.forEach { word: String ->
+            txt += if (txt === "") this.translit(word) else divider + this.translit(word)
+        }
+//        val(firstName,lastName) = parseFullName(payload)
+//        return "${this.translit(firstName)}$divider${this.translit(lastName)}"
+        return txt
     }
 
-   private fun translit(string: String?): String {
+    private fun translit(string: String?): String {
 
-       var tmpString = ""
+        var tmpString = ""
 
         val key = mapOf(
             "а" to "a", "б" to "b", "в" to "v", "г" to "g", "д" to "d", "е" to "e", "ё" to "e", "ж" to "zh", "з" to "z",
@@ -43,10 +50,17 @@ object Utils {
             "щ" to "sh'", "ъ" to "", "ы" to "i", "ь" to "", "э" to "e", "ю" to "yu", "я" to "ya"
         )
 
-       string?.forEach { c: Char ->
-        tmpString += key[c.toString().toLowerCase()] ?: c
-       }
+        string?.forEach { c: Char ->
+            //            tmpString += key[c.toString().toLowerCase()] ?: c
+            tmpString += when {
+                key[c.toString()] != null -> key[c.toString()]
+                key[c.toString().toLowerCase()] != null -> key[c.toString().toLowerCase()]?.toUpperCase()
+                else -> c
+            }
 
-       return tmpString.capitalize()
+        }
+
+        return tmpString
+//        return tmpString.capitalize()
     }
 }
